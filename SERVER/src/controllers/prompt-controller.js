bf0a4a3ad4787097c25af9ed219f1f4d18172108
Marilise Morona <marilise.morona@zarpsystem.com.br>
2023-03-08 22:27:45 -0300

    Feat-back: consumed api

---
+++ SERVER/src/controllers/prompt-controller.js
@@ -0,0 +1,25 @@
+const inputPrompt = require("../model/input-model")
+const openai = require("../config/openai")
+
+module.exports = {
+    async sendText(req, res){
+
+        const openAi  = openai.configuration()
+        const inputModel = new inputPrompt(req.body)
+
+        try {
+            const response = await openAi.createCompletion(
+                openai.textCompletion(inputModel)
+            )
+            return res.status(200).json({
+                sucess: true, 
+                data: response.data.choices[0].text
+            })
+        } catch (error) {
+            return res.status(400).json({
+                sucess: false,
+                error: error.response ? error.response.data : `Deu ruim no servidor`
+            })
+        }
+    }
+}
